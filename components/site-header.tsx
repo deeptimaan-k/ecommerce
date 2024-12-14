bf82a69c4fd63858b3ba45d021400f89b252e75c
Radiant <76729397+deeptimaan-k@users.noreply.github.com>
2024-12-14 11:30:13 +0530

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 const categories = [
-  // "Smartphones",
+  "Smartphones",
   // "Smart Watches",
   // "Laptops",
   // "Android TVs",
